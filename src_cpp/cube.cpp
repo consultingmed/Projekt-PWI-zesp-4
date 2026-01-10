a3d85bcb76933beb58bc6582f51445a223e3d3b8
devcpp832 <79039897+devcpp832@users.noreply.github.com>
2026-01-10 23:55:39 +0100

    Feat/cpp state and moves (#2)
    * Dodanie reprezentacji kostki oraz podstawowych ruchów: U, L, R, D, B, F
    
    * Poprawka w pliku cube.h - dopisanie metod: U(), R(), L(), D(), B(), F()
    
    * Poprawka poprawki - dodanie zapomnianych słów kluczowych void
    
    * Poprawienie logiki ruchów. Dodanie metody print(), która zamienia koordynaty Kociemby na opis kolorów na każdej ściance kostki i wypisuje ten opis na standardowe wyjście.
    
    * Ponowna poprawka w logice ruchów

---
+++ src_cpp/cube.cpp
@@ -0,0 +1,198 @@
+#include<bits/stdc++.h>
+#include "cube.h"
+#include "debug.h"
+using namespace std;
+cube::cube()
+{
+	iota(cp.begin(), cp.end(), 0);
+	iota(ep.begin(), ep.end(), 0);
+	for(auto &v : co) v = 0;
+	for(auto &v : eo) v = 0;
+}
+void cube::U() {
+    int swp = cp[3];
+    cp[3] = cp[2];
+    cp[2] = cp[1];
+    cp[1] = cp[0];
+    cp[0] = swp;
+
+    swp = ep[3];
+    ep[3] = ep[2];
+    ep[2] = ep[1];
+    ep[1] = ep[0];
+    ep[0] = swp;
+}
+
+void cube::D() {
+    int swp = cp[7];
+    cp[7] = cp[4];
+    cp[4] = cp[5];
+    cp[5] = cp[6];
+    cp[6] = swp;
+
+    swp = ep[11];
+    ep[11] = ep[8];
+    ep[8] = ep[9];
+    ep[9] = ep[10];
+    ep[10] = swp;
+
+}
+
+void cube::R() {
+    co[cp[2]] = (co[cp[2]]+2)%3;
+    co[cp[3]] = (co[cp[3]]+1)%3;
+    co[cp[6]] = (co[cp[6]]+1)%3;
+    co[cp[7]] = (co[cp[7]]+2)%3;
+
+    int swp = cp[3];
+    cp[3] = cp[7];
+    cp[7] = cp[6];
+    cp[6] = cp[2];
+    cp[2] = swp;
+
+    swp = ep[3];
+    ep[3] = ep[7];
+    ep[7] = ep[11];
+    ep[11] = ep[6];
+    ep[6] = swp;
+
+}
+
+void cube::L() {
+    co[cp[0]] = (co[cp[0]]+2)%3;
+    co[cp[1]] = (co[cp[1]]+1)%3;
+    co[cp[4]] = (co[cp[4]]+1)%3;
+    co[cp[5]] = (co[cp[5]]+2)%3;
+
+    int swp = cp[0];
+    cp[0] = cp[1];
+    cp[1] = cp[5];
+    cp[5] = cp[4];
+    cp[4] = swp;
+
+    swp = ep[1];
+    ep[1] = ep[5];
+    ep[5] = ep[9];
+    ep[9] = ep[4];
+    ep[4] = swp;
+
+}
+
+void cube::F() {
+    co[cp[0]] = (co[cp[0]]+1)%3;
+    co[cp[3]] = (co[cp[3]]+2)%3;
+    co[cp[4]] = (co[cp[4]]+2)%3;
+    co[cp[7]] = (co[cp[7]]+1)%3;
+
+    eo[ep[0]] ^= 1;
+    eo[ep[8]] ^= 1;
+    eo[ep[4]] ^= 1;
+    eo[ep[7]] ^= 1;
+
+    int swp = cp[0];
+    cp[0] = cp[4];
+    cp[4] = cp[7];
+    cp[7] = cp[3];
+    cp[3] = swp;
+
+    swp = ep[0];
+    ep[0] = ep[4];
+    ep[4] = ep[8];
+    ep[8] = ep[7];
+    ep[7] = swp;
+}
+
+void cube::B() {
+    co[cp[1]] = (co[cp[1]]+2)%3;
+    co[cp[2]] = (co[cp[2]]+1)%3;
+    co[cp[5]] = (co[cp[5]]+1)%3;
+    co[cp[6]] = (co[cp[6]]+2)%3;
+
+    eo[ep[2]] ^= 1;
+    eo[ep[6]] ^= 1;
+    eo[ep[10]] ^= 1;
+    eo[ep[5]] ^= 1;
+
+    int swp = cp[1];
+    cp[1] = cp[2];
+    cp[2] = cp[6];
+    cp[6] = cp[5];
+    cp[5] = swp;
+
+    swp = ep[2];
+    ep[2] = ep[6];
+    ep[6] = ep[10];
+    ep[10] = ep[5];
+    ep[5] = swp;
+
+}
+void cube::print()
+{
+	vector<pair<char, char> > edges = {
+	{'W', 'B'}, {'W', 'R'}, {'W', 'G'}, {'W', 'O'},
+	{'B', 'R'}, {'G', 'R'}, {'G', 'O'}, {'B', 'O'},
+	{'Y', 'B'}, {'Y', 'R'}, {'Y', 'G'}, {'Y', 'O'}};
+
+	vector<vector<char> > corners = {
+	{'W', 'R', 'B'}, {'W', 'G', 'R'}, {'W', 'O', 'G'}, {'W', 'B', 'O'},
+	{'Y', 'B', 'R'}, {'Y', 'R', 'G'}, {'Y', 'G', 'O'}, {'Y', 'O', 'B'}};
+
+	auto p_cor = [&](int num, int face)
+	{
+		cout<<corners[cp[num]][(co[cp[num]]+face)%3]<<" ";
+	};
+	auto p_edg = [&](int num, int flip)
+	{
+		flip ^= eo[ep[num]];
+		if(!flip)
+			cout<<edges[ep[num]].first<<" ";
+		else	
+			cout<<edges[ep[num]].second<<" ";
+	};
+	//Górna ściana:
+	cout<<"U: ";
+	p_cor(1, 0); p_edg(2, 0); p_cor(2, 0); cout<<endl;
+	p_edg(1, 0); cout<<"W "; p_edg(3, 0); cout<<endl;
+	p_cor(0, 0); p_edg(0, 0); p_cor(3, 0); cout<<endl;
+
+	//Dolna ściana:
+	cout<<"D: ";
+	p_cor(4, 0); p_edg(8, 0); p_cor(7, 0); cout<<endl;
+	p_edg(9, 0); cout<<"Y "; p_edg(11, 0); cout<<endl;
+	p_cor(5, 0); p_edg(10, 0); p_cor(6, 0); cout<<endl;
+
+	//Przednia ściana:
+	cout<<"F: ";
+	p_cor(0, 2); p_edg(0, 1); p_cor(3, 1); cout<<endl;
+	p_edg(4, 0); cout<<"B "; p_edg(7, 0); cout<<endl;
+	p_cor(4, 1); p_edg(8, 1); p_cor(7, 2); cout<<endl;
+
+	//Tylnia ściana:
+	cout<<"B: ";	
+	p_cor(5, 2); p_edg(10, 1); p_cor(6, 1); cout<<endl;
+	p_edg(5, 0); cout<<"G "; p_edg(6, 0); cout<<endl;
+	p_cor(1, 1); p_edg(2, 1); p_cor(2, 2); cout<<endl;
+
+	//Lewa ściana:
+	cout<<"L: ";
+	p_cor(1, 2); p_edg(1, 1); p_cor(0, 1); cout<<endl;
+	p_edg(5, 1); cout<<"R "; p_edg(4, 1); cout<<endl;
+	p_cor(5, 1); p_edg(9, 1); p_cor(4, 2); cout<<endl;
+
+	//Prawa ściana:
+	cout<<"R: ";
+	p_cor(3, 2); p_edg(3, 1); p_cor(2, 1); cout<<endl;
+	p_edg(7, 1); cout<<"O "; p_edg(6, 1); cout<<endl;
+	p_cor(7, 1); p_edg(11, 1); p_cor(6, 2); cout<<endl;
+
+	//Dokładna orientacja ścianki będzie zależeć specyfikacji programu wizualizującego w pythonie
+}
+void cube::move(int id) {
+    if(id==0) U();
+    if(id==1) D();
+    if(id==2) R();
+    if(id==3) L();
+    if(id==4) F();
+    if(id==5) B();
+}
+
---
+++ src_cpp/cube.h
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOVES 6
+class cube
+{
+public:
+	array<int, 8> cp; //permutacja rogow
+	array<int, 8> co; //orientacja rogow
+	array<int, 12> ep; //permutacja krawedzi
+	array<int, 12> eo; //orientacja krawedzi
+	//kostka jest reprezentowana wzgledem orientacji: niebieski center z przodu, biały center u góry, pomarańczowy center z prawej
+	cube();
+	
+	void R();
+	void L();
+	void U();
+	void D();
+	void B();
+	void F();
+	
+	void print();
+	void move(int id);
+};
